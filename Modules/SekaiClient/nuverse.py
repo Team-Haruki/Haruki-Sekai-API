d404f3f79e12364de0dbe598cdab7df3eca6f5f2
星雲希凪 <70424266+MejiroRina@users.noreply.github.com>
2025-07-07 21:02:05 +0800

    [Fix] Python is awesome

--- Modules/SekaiClient/nuverse.py
+++ Modules/SekaiClient/nuverse.py
@@ -206,7 +206,7 @@ structures = {
                 [
                     "eventExchangeCost",
                     (
-                        "resourceQuantity"
+                        "resourceQuantity",
                     )
                 ]
             ]
