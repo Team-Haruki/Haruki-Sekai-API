5ce6a6ca0526795956283de15693d8730a18f114
星雲希凪 <70424266+MejiroRina@users.noreply.github.com>
2025-09-25 02:52:06 +0800

    Add sekai cryptor

--- client/struct.go
+++ client/struct.go
@@ -1,1 +1,11 @@
 package client
+
+type SekaiRegion string
+
+const (
+	SekaiRegionJP SekaiRegion = "jp"
+	SekaiRegionEN SekaiRegion = "en"
+	SekaiRegionTW SekaiRegion = "tw"
+	SekaiRegionKR SekaiRegion = "kr"
+	SekaiRegionCN SekaiRegion = "cn"
+)
